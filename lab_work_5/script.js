31a2e093a0d4e6513cf31bbd5c3c9d85ee00c8e8
nikitagordeev10 <nikitagordeev10@yandex.ru>
2022-11-17 21:21:13 +0300

    Собрал наработки в папку. Сейчас буду объединять.

--- lab_work_5/script.js
+++ lab_work_5/script.js
@@ -12,7 +12,7 @@ function addInput() {
     var profile = document.getElementById('profile');
     var div = document.createElement('div');
     div.id = 'input' + ++x;
-    div.innerHTML = '<p>Телефон: <input type="phone" class="link" placeholder="+1(234)567-89-10"> </p>';
+    div.innerHTML = '<p>Телефон: <input type="tel" id="phone" name="phone" placeholder="+1(234)567-89-10" pattern="[+]{1}[0-9]{11,14}" required> </p>';
     profile.appendChild(div);
 }
 
@@ -20,4 +20,22 @@ function delInput() {
     var div = document.getElementById('input' + x);
     div.remove();
     --x;
+}
+
+
+// Вывод информации о пользователе
+function btnClick() {
+    var Txt1 = "";
+    var Txt2 = "";
+    Txt1 = document.Test.bt.value;
+    Txt2 = document.Test.bt.name;
+    document.getElementById('ex1').innerHTML = "<HR>" +
+        "<p>Вы нажали кнопку: " + Txt1.bold() +
+        " с именем: " + Txt2.bold() + "<\p>" + "<HR>";
+}
+
+//
+function asdfClick() {
+
+    document.querySelector('#welcome').innerHTML = '<b>Гость</b>, добро пожаловать на сайт!';
 }
